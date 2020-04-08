dce6df753683cc1d676f992cf7fcdfee8f9a568b
Xolo <solomonmanyaga@gmail.com>
2020-04-08 08:31:30 -0400

    Login form done!

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 
 import GreetingContainer from "./greeting/greeting_container";
+import SignupFormContainer from './session/signup_form_container';
+import LoginFormContainer from './session/login_form_container';
 
 const App = () => (
     <div>
@@ -8,6 +11,8 @@ const App = () => (
             <h1 className='header'>Welcome to LivePolls</h1>
             <GreetingContainer />
         </header>
+        <Route path="/login" component={LoginFormContainer} />
+        <Route path="/signup" component={SignupFormContainer} />
     </div>
 );
 
