dfe46ab4ee48e676ee9926561e790958f772a1b8
Xolo <solomonmanyaga@gmail.com>
2020-04-08 11:31:55 -0400

    Auth & Protected Routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -6,15 +6,20 @@ import SignupFormContainer from './session/signup_form_container';
 import LoginFormContainer from './session/login_form_container';
 import Polls  from './polls/polls_index';
 
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
+
 const App = () => (
     <div>
         <header>
             <h1 className='header'>Welcome to LivePolls</h1>
             <GreetingContainer />
         </header>
-        <Route path="/login" component={LoginFormContainer} />
-        <Route path="/signup" component={SignupFormContainer} />
-        <Route path="/polls" component={Polls} />
+        {/* <Route path="/login" component={LoginFormContainer} /> */}
+        {/* <Route path="/signup" component={SignupFormContainer} /> */}
+        <AuthRoute path="/login" component={LoginFormContainer} />
+        <AuthRoute path="/signup" component={SignupFormContainer} />
+        <ProtectedRoute path="/polls" component={Polls} />
+        {/* <Route path="/polls" component={Polls} /> */}
     </div>
 );
 
